fc9ae209c03b7629a65be3f88b679f49efee8827
fluchon <85443672+fluchon@users.noreply.github.com>
2024-11-17 10:02:23 +0100

    Add files via upload

---
+++ fotomatique/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fotomatique</title>
+    <link rel="stylesheet" href="css/damion.css">
+    <link rel="stylesheet" href="css/poppins.css">
+    <link rel="stylesheet" href="css/roboto.css">
+    <link rel="stylesheet" href="css/rubik.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div class="content">
+        <a href="index.html" class="desktop logo">Fotomatic</a>
+        <nav class="desktop">
+            <ul>
+                <li><a href="#">Product detail</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="https://www.instagram.com/" target="_blank">Follow us <img class="icon" src="images/instagram.webp" alt=""></a></li>
+                <li><a href="#">Join us</a></li>
+            </ul>
+        </nav>
+    </div>
+    <nav class="mobile">
+        <ul>
+            <li><a href=""><img src="images/ic-logo.svg" alt=""></a></li>
+            <li><a href=""><img src="images/ic-product-detail.svg" alt=""></a></li>
+            <li><a href=""><img src="images/ic-about-us.svg" alt=""></a></li>
+            <li><a href="" class="button">Join us</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main class="main-content">
+    <section id="sign-up-section" class="banner">
+        <div id="sign-up-cta">
+            <div class="content center">
+                <div class="header">
+                    <h1 class="cursive">Instant</h1>
+                    <h2 class="striking">FORMAT CAMERA</h2>
+                </div>
+                <div class="email">
+                    <span>Email us to request a demo and be in our waiting list for the <strong>febuary 2017</strong>release</span>
+                    <div class="button">join the waiting list</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="features-section">
+        <div class="feature">
+            <div class="center">
+                <div class="images-container">
+                    <img src="images/feature-1.webp" alt="">
+                </div>
+                <div class="content">
+                    <h2>Beautiful, crafted inside-out</h2>
+                    <h3>From the paint outside to the tiny screw inside, Fotomatic is crafted with love and 20-year of expertise</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="filters-section">
+        <div class="content center">
+            <h2>Over 20+ filters to choose from</h2>
+            <h3>feed your creativity with 20 different filter designed by our eclectic in-house photographers!</h3>
+        </div>
+        <div class="images-container">
+            <div class="image-container">
+                <img src="images/filter-1.webp" alt="">
+            </div>
+            <div class="image-container">
+                <img src="images/filter-2.webp" alt="">
+            </div>
+            <div class="image-container">
+                <img src="images/filter-3.webp" alt="">
+            </div>
+        </div>
+    </section>
+    <section id="quotes-section">
+        <div class="content center"></div>
+        <span class="quote-citation">"it's truly something that could create a Cbrand new photography renaissance</span>
+        <img class="quote-citation" src="images/photography-logo.webp" alt="">
+    </section>
+</main>
+<footer>
+    <div class="content">
+        <span class="copyright">2016 Fotomatique, All Rights Reserved</span>
+        <span class="location"> Designed in NYC</span>
+    </div>
+</footer>
+
+</body>
+</html>
